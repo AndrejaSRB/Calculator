c222442a146dd9230d4e6d4d4673c289fdf6a6db
AndrejaSRB <andreja.kojadinovic@gmail.com>
2018-11-07 14:37:32 +0100

    first commit

---
+++ src/components/elements/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Button.css';
+
+const isOperator = (val) => {
+    return !isNaN(val) || val === "." || val === "=";
+}
+// We are asking if val is not a number or "." or "=".
+
+
+const Button = (props) => <div className={`button-wrapper ${
+    isOperator(props.children) ? null : 'operator'
+}`} onClick={() => props.handleClick(props.children)}>{props.children}</div>
+
+// We are asking if it's operator or no. If it's then we are adding clas "operator".
+
+export default Button
+
